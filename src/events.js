efcef955030fdf5ae7b0bcc4e73eb9ddd70a46dc
rongxanh88 <rongxanh88@gmail.com>
2017-09-05 09:35:09 -0600

    refactor other methods out of events

--- src/events.js
+++ src/events.js
@@ -1,36 +1,10 @@
 const view = require('./view-helper')
-const baseUrl = 'https://wordwatch-api.herokuapp.com'
 
+const baseUrl = 'https://wordwatch-api.herokuapp.com'
 const topWordUrl = '/api/v1/top_word'
+
+const $submitBtn = $('.text-submission button')
 
 $.get(baseUrl + topWordUrl, view.topWordHTML)
 
-$('.text-submission button').on('click', () => {
-  const text = $('.text-submission textarea')[0].value
-  const words = text.split(' ')
-
-  let wordFrequency = {}
-
-  words.forEach((word) => {
-    word = word.toLowerCase()
-    if (wordFrequency[word]) {
-      wordFrequency[word] = wordFrequency[word] + 1
-    } else {
-      wordFrequency[word] = 1
-    }
-  })
-
-  const wordFrequencyKeys = Object.keys(wordFrequency)
-
-  wordFrequencyKeys.forEach((word) => {
-    $('.word-count').append(`<span style="font-size: ${wordFrequency[word]}em">${word}&nbsp;</span>`)
-  })
-
-  wordFrequencyKeys.forEach((word) => {
-    const postUrl = '/api/v1/words'
-    $.post(baseUrl + postUrl, { "word": { "value": word }})
-      .then((data) => {
-        console.log(data)
-      })
-  })
-})
+$submitBtn.on('click', view.displayAnalyzedText)
--- src/view-helper.js
+++ src/view-helper.js
@@ -6,4 +6,35 @@ const topWordHTML = (data, status) => {
   $('.word-count').appendHTML
 }
 
-module.exports = { topWordHTML }
+const displayAnalyzedText = () => {
+  const baseUrl = 'https://wordwatch-api.herokuapp.com'
+  const text = $('.text-submission textarea')[0].value
+  const words = text.split(' ')
+  
+  let wordFrequency = {}
+  
+  words.forEach((word) => {
+    word = word.toLowerCase()
+    if (wordFrequency[word]) {
+      wordFrequency[word] = wordFrequency[word] + 1
+    } else {
+      wordFrequency[word] = 1
+    }
+  })
+  
+  const wordFrequencyKeys = Object.keys(wordFrequency)
+  
+  wordFrequencyKeys.forEach((word) => {
+    $('.word-count').append(`<span style="font-size: ${wordFrequency[word]}em">${word}&nbsp;</span>`)
+  })
+  
+  wordFrequencyKeys.forEach((word) => {
+    const postUrl = '/api/v1/words'
+    $.post(baseUrl + postUrl, { "word": { "value": word }})
+      .then((data) => {
+        console.log(data)
+      })
+  })
+}
+
+module.exports = { topWordHTML, displayAnalyzedText }
