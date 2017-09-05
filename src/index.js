ad55df4039abce91b0a1889d6d4027f6c46858d1
rongxanh88 <rongxanh88@gmail.com>
2017-09-05 07:44:16 -0600

    initial commit - start of assessment

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // have fun!
 })
 
+// As a user,  
+// when I visit Word Watch  
+// I should see the top used word and its count (based on the Word Watch API database)  
+// in the "Top Word: " heading.  
+// e.g., "Top Word: ciabatta (22)" 
